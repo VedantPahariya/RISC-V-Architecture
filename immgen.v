d226671e41c9797ad827801941165dcbae310520
sarvesh <sarvesh.takbhate@students.iiit.ac.in>
2025-02-21 16:45:47 +0530

    hogaya

--- immgen.v
+++ immgen.v
@@ -3,7 +3,6 @@ module imm_gen(
     output [63:0] imm
 );
 
-    //we want to  concatenate the msb of instruction till it becoms 64 bit and front
     assign imm = {{52{instruction[31]}},instruction[31],instruction[7], instruction[30:25], instruction[11:8]};
 
 endmodule
--- ops.v
+++ ops.v
@@ -65,6 +65,7 @@ module fa(a,b,c,sum,carry);
 
 endmodule
 
+
 module ADD(rs1,rs2,rd,cin,zero_flag,carry_flag,overflow_flag);
 
     input signed [63:0] rs1,rs2;
@@ -115,31 +116,120 @@ module SUB(rs1,rs2,rd,zero_flag,carry_flag,overflow_flag);
 
 endmodule
 
-module SLL (rs1,rs2,rd);
+// module SLL (
+//     input signed [63:0] rs1, rs2,
+//     output signed [63:0] rd,
+//     output reg overflow_flag,
+//     output reg carry_flag
+// );
 
-    input signed [63:0] rs1,rs2;
-    output signed [63:0] rd;
+//     wire [5:0] shift;
+//     assign shift = rs2[5:0];
+
+//     wire [63:0] stage1, stage2, stage3, stage4, stage5, stage6;
+
+
+//     assign stage1 = shift[0] ? {rs1[62:0], 1'b0} : rs1;
+//     assign stage2 = shift[1] ? {stage1[61:0], 2'b0} : stage1;
+//     assign stage3 = shift[2] ? {stage2[59:0], 4'b0} : stage2;
+//     assign stage4 = shift[3] ? {stage3[55:0], 8'b0} : stage3;
+//     assign stage5 = shift[4] ? {stage4[47:0], 16'b0} : stage4;
+//     assign stage6 = shift[5] ? {stage5[31:0], 32'b0} : stage5;
+
+//     assign rd = stage6;
+
+//     // Carry Flag: Check if any bit that was shifted out was 1
+//     always @(*) begin
+//         carry_flag = 0;
+//         case (shift)
+//             6'd1: carry_flag = rs1[63];             
+//             6'd2: carry_flag = |rs1[63:62];        
+//             6'd4: carry_flag = |rs1[63:60];        
+//             6'd8: carry_flag = |rs1[63:56];         
+//             6'd16: carry_flag = |rs1[63:48];        
+//             6'd32: carry_flag = |rs1[63:32];   
+//             default: carry_flag = 0;
+//         endcase
+//     end
+
+//     always @(*) begin
+//         overflow_flag = xor x12(rs1[63], rd[63]);
+//     end
+
+// endmodule
+
+
+module SLL(
+    input signed [63:0] rs1,
+    input signed [63:0] rs2,  
+    output reg overflow_flag  
+    output signed [63:0] rd,
+    output reg carry_flag
+);
+
     wire [5:0] shift;
     assign shift = rs2[5:0];
- 
-    wire [63:0] stage1, stage2, stage3, stage4, stage5;
+
+     wire [63:0] stage1, stage2, stage3, stage4, stage5, stage6;
+
 
     assign stage1 = shift[0] ? {rs1[62:0], 1'b0} : rs1;
     assign stage2 = shift[1] ? {stage1[61:0], 2'b0} : stage1;
     assign stage3 = shift[2] ? {stage2[59:0], 4'b0} : stage2;
     assign stage4 = shift[3] ? {stage3[55:0], 8'b0} : stage3;
     assign stage5 = shift[4] ? {stage4[47:0], 16'b0} : stage4;
-    assign rd = shift[5] ? {stage5[31:0], 32'b0} : stage5;
+    assign stage6 = shift[5] ? {stage5[31:0], 32'b0} : stage5;
+
+    assign rd = stage6;
+
+    wire signed [63:0] largest_pos, largest_neg;
+    wire signed [63:0] pos_threshold, neg_threshold;
+    wire signed [63:0] pos_check, neg_check;
+    
+    wire temp1;
+    wire temp2;
+
+    assign largest_pos = 64'h7FFF_FFFF_FFFF_FFFF;  
+    assign largest_neg = 64'h8000_0000_0000_0000;  
+
+    SRL shift_right_pos(largest_pos, rs2, pos_threshold,temp1,temp2);
+    SRL shift_right_neg(largest_neg, rs2, neg_threshold,temp1,temp2);
+
+    SLT signed_compare(rs1, pos_threshold, pos_check);
+    SLT signed_compare_neg(rs1, neg_threshold, neg_check);
+
+    always @(*) begin
+        if (rs1 >= 0) 
+            overflow_flag = pos_check[0]; 
+        else 
+            overflow_flag = neg_check[0]; 
+    end
+
+
+    always @(*) begin
+        carry_flag = 0;
+        case (shift)
+            6'd1: carry_flag = rs1[63];             
+            6'd2: carry_flag = |rs1[63:62];        
+            6'd4: carry_flag = |rs1[63:60];        
+            6'd8: carry_flag = |rs1[63:56];         
+            6'd16: carry_flag = |rs1[63:48];        
+            6'd32: carry_flag = |rs1[63:32];   
+            default: carry_flag = 0;
+        endcase
+    end
+
+
 
 endmodule
 
-module SRL (rs1,rs2,rd);
-
-    input signed [63:0] rs1,rs2;
+module SRL (rs1, rs2, rd,carry_flag,overflow_flag);
+    input signed [63:0] rs1, rs2;
     output signed [63:0] rd;
+
     wire [5:0] shift;
     assign shift = rs2[5:0];
- 
+
     wire [63:0] stage1, stage2, stage3, stage4, stage5;
 
     assign stage1 = shift[0] ? {1'b0, rs1[63:1]} : rs1;
@@ -147,17 +237,24 @@ module SRL (rs1,rs2,rd);
     assign stage3 = shift[2] ? {4'b0, stage2[63:4]} : stage2;
     assign stage4 = shift[3] ? {8'b0, stage3[63:8]} : stage3;
     assign stage5 = shift[4] ? {16'b0, stage4[63:16]} : stage4;
-    assign rd = shift[5] ? {32'b0, stage5[63:32]} : stage5;
+    assign rd    = shift[5] ? {32'b0, stage5[63:32]} : stage5;
+
+    assign carry_flag = |rs1[63-shift-1:0];
+
+    assign overflow_flag = xor x12(rs1[63],rd[63]);
+
 
 endmodule
 
-module SRA (rs1,rs2,rd);
 
-    input signed [63:0] rs1,rs2;
+module SRA (rs1, rs2, rd, carry_flag,overflow_flag);
+    input signed [63:0] rs1, rs2;
     output signed [63:0] rd;
+    output carry_flag;
+
     wire [5:0] shift;
     assign shift = rs2[5:0];
- 
+
     wire [63:0] stage1, stage2, stage3, stage4, stage5;
 
     assign stage1 = shift[0] ? {rs1[63], rs1[63:1]} : rs1;
@@ -165,9 +262,14 @@ module SRA (rs1,rs2,rd);
     assign stage3 = shift[2] ? {{4{rs1[63]}}, stage2[63:4]} : stage2;
     assign stage4 = shift[3] ? {{8{rs1[63]}}, stage3[63:8]} : stage3;
     assign stage5 = shift[4] ? {{16{rs1[63]}}, stage4[63:16]} : stage4;
-    assign rd = shift[5] ? {{32{rs1[63]}}, stage5[63:32]} : stage5;
+    assign rd    = shift[5] ? {{32{rs1[63]}}, stage5[63:32]} : stage5;
+
+    assign carry_flag = |rs1[shift-1:0];
+
+    assign overflow_flag = 1'b0;
 
 endmodule
+
 
 
 module SLT(rs1,rs2,rd);
