89b3547fc2c8fd519ec2cb8459450b40aa8b41b0
Srihari <srihari.padmanabhan@students.iiit.ac.in>
2025-02-21 16:38:11 +0530

    pushing seq block

--- imm_mux.v
+++ imm_mux.v
@@ -1,8 +1,8 @@
-module(
-    input [63:0] rs2;
-    input [63:0] imm;
-    output [63:0] res;
-    input alu_src;
+module imm_mux(
+    input [63:0] rs2,
+    input [63:0] imm,
+    output [63:0] res,
+    input alu_src
 );
 
     assign res = alu_src ? imm : rs2;
--- writeback_mux.v
+++ writeback_mux.v
@@ -1,8 +1,8 @@
-module(
-    input [63:0] alu_out;
-    input [63:0] mem_out;
-    output [63:0] writeback;
-    input MemtoReg;
+module wb_mux(
+    input [63:0] alu_out,
+    input [63:0] mem_out,
+    output [63:0] writeback,
+    input MemtoReg
 );
 
     assign writeback = MemtoReg ? mem_out : alu_out;
