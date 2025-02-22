1c0ee7b27e07f33b747a2ac6ee7d30e142047c8f
Srihari <srihari.padmanabhan@students.iiit.ac.in>
2025-02-22 21:03:24 +0530

    added checker

--- registers.v
+++ registers.v
@@ -25,6 +25,7 @@ module register(
         if(RegWrite)
         begin
             register[rd] = wrt_data;
+            $display("Register %d updated to %d", rd, wrt_data);
             $writememh("reglog.mem", register);  // Save updated registers to file
         end
     end    
