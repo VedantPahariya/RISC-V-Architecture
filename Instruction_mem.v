edbb1623b93c52b1c2dff53e0f4529379f5723a0
Vedant Pahariya <vedant.pahariya@research.iiit.ac.in>
2025-02-22 19:55:05 +0530

    updated instruction memory

--- Instruction_mem.v
+++ Instruction_mem.v
@@ -18,7 +18,7 @@ module insmem(
     end
     // access the memory location indicated by the 8 bit address
 
-    // reg [7:0] address = 8'h00;
+    //  reg [7:0] address = 8'h0;
 
        assign ctrl = memory[address][6:0];
        assign rd = {memory[address+1][3:0],memory[address][7]};
@@ -33,10 +33,36 @@ module insmem(
     //     #1
     //     $display("Address: %h", address);
     //     $display("Instruction: %h", instruction);
-    //     $display("Control: %b", ctrl);
+    //     $display("Opcode: %b", ctrl);
     //     $display("rd: %b", rd);
     //     $display("rs1: %b", rs1);
     //     $display("rs2: %b", rs2);
     // end
+    
+    always @(*) begin
+        
+        case (ctrl)
+            7'b0010011: begin // I-format (for immediate add)
+                $display("I-format with opcode 0010011");
+                $display("adding rs1=%d with imm=%d in rd=%d \t \t x%d = x%d + %d", rs1, instruction[31:20], rd, rd, rs1, instruction[31:20]);
+            end
+            7'b0000011: begin // Load (ld)
+                $display("Load with opcode 0000011");
+                $display("loading from 'address in rs1'=%d with offset imm= %d in rd=%d", rs1, instruction[31:20], rd);
+            end
+            7'b0100011: begin // Store (sd)
+                $display("Store with opcode 0100011");
+                $display("storing to 'address in rs1'=%d with offset imm= %d from rs2=%d", rs1, {{52{instruction[31]}},instruction[31:25], instruction[11:7]}, rs2);
+            end
+            7'b1100011: begin // Branch (beq)
+                $display("Branch with opcode 1100011");
+                $display("branching if rs1=%d is equal to rs2=%d to PC = %d + 2*imm= %d", rs1, rs2,address,{{52{instruction[31]}},instruction[31],instruction[7], instruction[30:25], instruction[11:8]});
+            end
+            default: begin
+                // Default case to handle undefined opcodes
+                $display("Undefined opcode");
+            end
+        endcase
+    end
 
 endmodule
--- immgen.v
+++ immgen.v
@@ -3,6 +3,31 @@ module imm_gen(
     output [63:0] imm
 );
 
-    assign imm = {{52{instruction[31]}},instruction[31],instruction[7], instruction[30:25], instruction[11:8]};
+    wire opcode;
+    reg [63:0] immediate;
+    assign opcode = instruction[6:0];
+    always @(*) begin
+        
+        case (ctrl)
+            7'b0010011: begin // I-format (for immediate add)
+                immediate = {{52{instruction[31]}},instruction[31:20]};
+            end
+            7'b0000011: begin // Load (ld)
+                immediate = {{52{instruction[31]}},instruction[31:20]};
+            end
+            7'b0100011: begin // Store (sd)
+                immediate = {{52{instruction[31]}},instruction[31:25], instruction[11:7]};
+            end
+            7'b1100011: begin // Branch (beq)
+                immediate = {{52{instruction[31]}},instruction[31],instruction[7], instruction[30:25], instruction[11:8]};
+            end
+            default: begin
+                // Default case to handle undefined opcodes
+                immediate = 64'b0;
+            end
+        endcase
+    end
+
+    assign imm = immediate;
 
 endmodule
