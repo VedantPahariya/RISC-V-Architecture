cafdea4863ade17a987712cd42c606468f65e892
Srihari <srihari.padmanabhan@students.iiit.ac.in>
2025-03-07 02:34:03 +0530

    adding hazard correction units

---
+++ Pipeline/Execute/forward_muxes.v
@@ -0,0 +1,31 @@
+module forward_mux_a(
+input signed [63:0] ID_EX_rs1_value,
+input signed [63:0] ID_EX_rs2_value,
+input signed [63:0] EX_MEM_rd_value,
+input signed [63:0] writeback_mux_value,
+input [1:0] ForwardA,
+input [1:0] ForwardB,
+output signed [63:0] alu_in_A;
+output signed [63:0] alu_in_B;
+);
+
+        reg signed [63:0] alu_in_A_reg;
+        reg signed [63:0] alu_in_B_reg;
+
+        always @(*) begin
+            case (ForwardA)
+                2'b00: alu_in_A_reg = ID_EX_rs1_value;
+                2'b01: alu_in_A_reg = writeback_mux_value;
+                2'b10: alu_in_A_reg = EX_MEM_rd_value;
+            endcase
+            case (ForwardB)
+                2'b00: alu_in_B_reg = ID_EX_rs2_value;
+                2'b01: alu_in_B_reg = writeback_mux_value;
+                2'b10: alu_in_B_reg = EX_MEM_rd_value;
+            endcase
+        end
+
+        assign alu_in_A = alu_in_A_reg;
+        assign alu_in_B = alu_in_B_reg;
+        
+endmodule 
--- Pipeline/hazard_detection.v
+++ Pipeline/hazard_detection.v
@@ -3,107 +3,21 @@ module hazard_detection_unit(
     input [4:0] IF_ID_rs2,
     input [4:0] ID_EX_rd,
     input ID_EX_MemRead,
-    input [6:0] ctrl, // Opcode input to the control unit
     output reg stall,
     output reg IF_ID_Write,
     output reg PC_Write,
-    output reg branch,
-    output reg RegWrite,
-    output reg MemtoReg,
-    output reg MemRead,
-    output reg MemWrite,
-    output reg alu_src,
-    output reg [1:0] alu_op
 );
 
     //this checks for stalling 
     always @(*) begin
         if (((IF_ID_rs1 == ID_EX_rd) || (IF_ID_rs2 == ID_EX_rd)) && ID_EX_MemRead && (ID_EX_rd != 0))
             stall = 1;  // Stall detected
+            IF_ID_Write = 0;  
+            PC_Write = 0;
         else
             stall = 0;  // No hazard, proceed normally
-    end
-
-    //stopping writting and incrementing the PC
-    always @(*) begin
-        if (stall) begin
-            IF_ID_Write = 0;  
-            PC_Write = 0;     
-        end else begin
             IF_ID_Write = 1;  
             PC_Write = 1;
-        end
-    end
-
-    //now if stalling is there then we have to stop all the operations , so accordingly we do it.
-    always @(*) begin
-        if (stall) begin
-            branch = 0;
-            RegWrite = 0;
-            MemtoReg = 0;
-            MemRead = 0;
-            MemWrite = 0;
-            alu_src = 0;
-            alu_op = 2'b00;
-            
-        end else begin
-            case (ctrl)
-                7'b0110011: begin // R-format
-                    alu_src = 0;
-                    MemtoReg = 0;
-                    RegWrite = 1;
-                    MemRead = 0;
-                    MemWrite = 0;
-                    branch = 0;
-                    alu_op = 2'b10;
-                end
-                7'b0010011: begin // I-format (Immediate add)
-                    alu_src = 1;
-                    MemtoReg = 0;
-                    RegWrite = 1;
-                    MemRead = 0;
-                    MemWrite = 0;
-                    branch = 0;
-                    alu_op = 2'b00;
-                end
-                7'b0000011: begin // Load (ld)
-                    alu_src = 1;
-                    MemtoReg = 1;
-                    RegWrite = 1;
-                    MemRead = 1;
-                    MemWrite = 0;
-                    branch = 0;
-                    alu_op = 2'b00;
-                end
-                7'b0100011: begin // Store (sd)
-                    alu_src = 1;
-                    MemtoReg = 1'bx; // Don't care
-                    RegWrite = 0;
-                    MemRead = 0;
-                    MemWrite = 1;
-                    branch = 0;
-                    alu_op = 2'b00;
-                end
-                7'b1100011: begin // Branch (beq)
-                    alu_src = 0;
-                    MemtoReg = 1'bx; // Don't care
-                    RegWrite = 0;
-                    MemRead = 0;
-                    MemWrite = 0;
-                    branch = 1;
-                    alu_op = 2'b01;
-                end
-                default: begin
-                    branch = 0;
-                    RegWrite = 0;
-                    MemtoReg = 0;
-                    MemRead = 0;
-                    MemWrite = 0;
-                    alu_src = 0;
-                    alu_op = 2'b00;
-                end
-            endcase
-        end
     end
 
 endmodule
